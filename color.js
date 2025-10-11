a0a5051b080ae6aa1aec2e7904892c6fb3373af2
Svetlana Lakhtik <163621329+tyubie@users.noreply.github.com>
2025-10-11 13:47:29 +0700

    javascript style alert

---
+++ color.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function()
+{
+let body = document.querySelector ("body");
+        document.querySelector("#red").addEventListener("click",
+            function(){ body.style.backgroundColor = "red";}
+        );
+        document.querySelector("#green").addEventListener("click",
+            function(){ body.style.backgroundColor = "green";}
+        );
+            document.querySelector("#blue").addEventListener("click",
+            function(){ body.style.backgroundColor = "blue";}
+        );
+})
